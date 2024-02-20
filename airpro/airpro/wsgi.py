e665dc35fe251ce3e57c25f74ef3f8b87c3f11c1
sagar <sagarc62@gmail.com>
2024-02-20 17:11:58 +0530

    wsgi commit by pushpa

--- airpro/airpro/wsgi.py
+++ airpro/airpro/wsgi.py
@@ -14,4 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airpro.settings')
 
 application = get_wsgi_application()
-#pushpa pushparah zukega nahi
+#jagruti my best friend
