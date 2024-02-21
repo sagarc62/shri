8f88189492d7d0bde1f702242a16c43977dc5fb2
pushparajpatil <pareshdx99@rediffmail.com>
2024-02-21 14:10:34 +0530

    changes in model and admin file

--- airpro/testapp/models.py
+++ airpro/testapp/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class  User(models.Model):
-    name=models.CharField(max_length=30)
+class Passenger(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email_id=models.EmailField(unique=True)
+    mobile_number=models.IntegerField()
+    age=models.PositiveSmallIntegerField()
+    gender=models.CharField(max_length=10,choices=(('Male','Male'),('Female','Female')))
