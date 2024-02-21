922fb50160b12b7f0218b35ec3acc5df24bd35e1
sagar <sagarc62@gmail.com>
2024-02-21 14:02:36 +0530

    database

--- airpro/airpro/settings.py
+++ airpro/airpro/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-#hello world
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +75,12 @@ WSGI_APPLICATION = 'airpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'root',
+        'USER':'sagarc62@gmail.com',
+        'PASSWORD':'8055343557',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
